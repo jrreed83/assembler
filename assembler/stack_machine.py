a9661c7c835ba76bdf80d1909d322371d503f738
jrreed83 <joeyrreed@gmail.com>
2017-12-22 16:17:04 -0800

    jrr

--- assembler/stack_machine.py
+++ assembler/stack_machine.py
@@ -1,7 +1,7 @@
 class Stack():
     def __init__(self, size=256):
         self.array = [0] * size
-        self.head  = 0
+        self.head = 0
 
 def push(stack, x):
     stack.head = stack.head+1
@@ -17,46 +17,46 @@ def peek(stack):
     return stack.array[stack.head]
 
 class OpCode:
-    NOP     = 0
-    HALT    = 1
-    IADD    = 2
-    ISUB    = 3
-    IMUL    = 4
+    NOP = 0
+    HALT = 1
+    IADD = 2
+    ISUB = 3
+    IMUL = 4
     ICONST0 = 5
     ICONST1 = 6
     ICONST2 = 7
-    PRINT   = 8
+    PRINT = 8
 
 
 class CPU():
     def __init__(self):
-        self.opStack = Stack()
+        self.op_stack = Stack()
         self.ip = 0
 
 def execute(cpu, instructions=[]):
     ip = cpu.ip
-    opStack = cpu.opStack
+    op_stack = cpu.op_stack
     op = instructions[ip]
-    while(op != OpCode.HALT):
+    while op != OpCode.HALT:
         ip = ip + 1
         if op == OpCode.NOP:
             pass
         elif op == OpCode.IADD:
-            x = pop(opStack)
-            y = pop(opStack)
-            push(opStack,x+y)
+            x = pop(op_stack)
+            y = pop(op_stack)
+            push(op_stack, x+y)
         elif op == OpCode.ICONST0:
-            push(opStack,0)  
+            push(op_stack, 0)  
         elif op == OpCode.ICONST1:
-            push(opStack,1)    
+            push(op_stack, 1)    
         elif op == OpCode.ICONST2:
-            push(opStack,2)                                             
+            push(op_stack, 2)                                             
         elif op == OpCode.ISUB:
-            x = pop(opStack)
-            y = pop(opStack)
-            push(opStack,x-y)              
+            x = pop(op_stack)
+            y = pop(op_stack)
+            push(op_stack, x-y)              
         elif op == OpCode.PRINT:
-            print(opStack.pop())
+            print(op_stack.pop())
 
         op = instructions[ip]
     
--- assembler/tokens.py
+++ assembler/tokens.py
@@ -1,22 +1,22 @@
 class Tag:
-    NOP     = 'NOP'
-    HALT    = 'HALT'
-    IADD    = 'IADD'
-    ISUB    = 'ISUB'
-    IMUL    = 'IMUL'
+    NOP = 'NOP'
+    HALT = 'HALT'
+    IADD = 'IADD'
+    ISUB = 'ISUB'
+    IMUL = 'IMUL'
     ICONST0 = 'ICONST0'
     ICONST1 = 'ICONST1'
     ICONST2 = 'ICONST2'
-    PRINT   = 'PRINT'
-    NUMBER  = 'NUMBER'
+    PRINT = 'PRINT'
+    NUMBER = 'NUMBER'
 
 class Token:
     def __init__(self, tag=Tag.NOP, txt=None):
-        self.tag = tag 
-        self.txt = txt 
+        self.tag = tag
+        self.txt = txt
     def __str__(self):
         if self.txt is not None:
-            return '<{0}, {1}>'.format(self.tag,self.txt)
+            return '<{0}, {1}>'.format(self.tag, self.txt)
         else:
             return '<{0}>'.format(self.tag)
     def __repr__(self):
