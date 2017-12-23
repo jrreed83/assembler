abde756a346d7db3816b7dfe85a533dce35fe364
jrreed83 <joeyrreed@gmail.com>
2017-12-22 22:43:36 -0800

    jrr

--- assembler/stack_machine.py
+++ assembler/stack_machine.py
@@ -16,46 +16,47 @@ def pop(stack):
 def peek(stack):
     return stack.array[stack.head]
 
-class OpCode:
-    NOP = 0
-    HALT = 1
-    IADD = 2
-    ISUB = 3
-    IMUL = 4
-    ICONST0 = 5
-    ICONST1 = 6
-    ICONST2 = 7
-    PRINT = 8
+
+NOP = 0
+HALT = 1
+IADD = 2
+ISUB = 3
+IMUL = 4
+ICONST0 = 5
+ICONST1 = 6
+ICONST2 = 7
+PRINT = 8
 
 
 class CPU():
     def __init__(self):
         self.op_stack = Stack()
         self.ip = 0
+        self.memory = {}
 
 def execute(cpu, instructions=[]):
     ip = cpu.ip
     op_stack = cpu.op_stack
     op = instructions[ip]
-    while op != OpCode.HALT:
+    while op != HALT:
         ip = ip + 1
-        if op == OpCode.NOP:
+        if op == NOP:
             pass
-        elif op == OpCode.IADD:
+        elif op == IADD:
             x_1 = pop(op_stack)
             x_2 = pop(op_stack)
             push(op_stack, x_1+x_2)
-        elif op == OpCode.ICONST0:
+        elif op == ICONST0:
             push(op_stack, 0)  
-        elif op == OpCode.ICONST1:
+        elif op == ICONST1:
             push(op_stack, 1)    
-        elif op == OpCode.ICONST2:
+        elif op == ICONST2:
             push(op_stack, 2)                                             
-        elif op == OpCode.ISUB:
+        elif op == ISUB:
             x_1 = pop(op_stack)
             x_2 = pop(op_stack)
             push(op_stack, x_1-x_2)              
-        elif op == OpCode.PRINT:
+        elif op == PRINT:
             print(op_stack.pop())
 
         op = instructions[ip]
--- assembler/tokens.py
+++ assembler/tokens.py
@@ -1,23 +1,69 @@
-class Tag:
-    NOP = 'NOP'
-    HALT = 'HALT'
-    IADD = 'IADD'
-    ISUB = 'ISUB'
-    IMUL = 'IMUL'
-    ICONST0 = 'ICONST0'
-    ICONST1 = 'ICONST1'
-    ICONST2 = 'ICONST2'
-    PRINT = 'PRINT'
-    NUMBER = 'NUMBER'
+import io
+
+RESERVED = [
+    'nop',
+    'halt',
+    'iadd',
+    'isub',
+    'imul',
+    'iconst0',
+    'iconst1',  
+    'iconst2',
+    'ipush',
+    'print'      
+]
+
+
 
 class Token:
-    def __init__(self, tag=Tag.NOP, txt=None):
+    def __init__(self, tag='NOP', txt=None):
         self.tag = tag
         self.txt = txt
     def __str__(self):
         if self.txt is not None:
-            return '<{0}, {1}>'.format(self.tag, self.txt)
+            return '[{0} {1}]'.format(self.tag, self.txt)
         else:
-            return '<{0}>'.format(self.tag)
+            return '[{0}]'.format(self.tag)
     def __repr__(self):
         return self.__str__()    
+
+class Lexer:
+    def __init__(self, stream = None, reserved = []):
+        self.sid = stream
+        self.reserved = reserved
+    def next_char(self):
+        return self.sid.read(1) 
+    def next_token(self):
+        peek = self.next_char() 
+        while peek.isspace():
+            peek = self.next_char()
+        if peek.isalpha():
+            buffer = peek
+            peek = self.next_char()
+            while peek.isalpha():
+                buffer += peek 
+                peek = self.next_char()
+            if buffer in self.reserved:
+                return Token(buffer.upper())
+            else: 
+                return Token('WORD', buffer)
+        elif peek.isdigit():
+            buffer = peek
+            peek = self.next_char()
+            while peek.isdigit():
+                buffer += peek 
+                peek = self.next_char()
+            return Token('NUMBER', buffer)           
+
+if __name__ == '__main__':
+    lexer = Lexer(io.StringIO('ipush 7 halt'), RESERVED)
+    word = lexer.next_token()
+    print(word)
+    word = lexer.next_token()
+    print(word)
+    word = lexer.next_token()
+    print(word)    
+        
+        
+
+
