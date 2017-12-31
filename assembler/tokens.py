7bcf4a605992b9df850ab9cf62b571daabab43b2
jrreed83 <joeyrreed@gmail.com>
2017-12-31 14:21:06 -0800

    limiting mutability

--- assembler/tokens.py
+++ assembler/tokens.py
@@ -151,13 +151,34 @@ def instruction_isub(assm):
     assm.ip += 1
     return True 
 
-# def instruction_ipush(assm):
-#     if reserved('ipush', assm):
-#         commit(assm)
-#         if integer(assm):
-#             return True
-#     rollback(assm)
-#     return False 
+def instruction_ipush(assm):
+    start = assm.start
+    [stop, token1] = reserved('ipush', assm.input, assm.start)
+    if token1 is not None:
+        [stop, token2] = integer(assm.input, stop)
+        if token2 is not None:
+            tokens = [token1] + token2
+            assm.start = stop
+            assm.pos = stop
+            assm.code += tokens
+            assm.ip += 5
+            return True 
+    return False
+
+def instruction_spush(assm):
+    start = assm.start
+    [stop, token1] = reserved('spush', assm.input, assm.start)
+    if token1 is not None:
+        [stop, token2] = string(assm.input, stop)
+        if token2 is not None:
+            index = len(assm.constants)            
+            assm.constants += [token2]
+            assm.code += [token1, index]   
+            assm.start = stop
+            assm.pos = stop 
+            assm.ip += 2
+            return True 
+    return False
 
 def instruction_iconst0(assm):
     [stop, token] = reserved('iconst0', assm.input, assm.start)
@@ -219,24 +240,28 @@ def instruction_print(assm):
     assm.ip += 1
     return True 
 
-def integer(assm):
-    white_space(assm)
-    if not peek(assm).isdigit():
-        return False
+def space(string, start = 0):
+    ptr = start
+    while string[ptr].isspace():
+        ptr += 1
+    return ptr
 
-    while next_char(assm).isdigit():
-        pass
-    rewind(assm)
-    value = int(assm.input[assm.start:assm.pos])
+def integer(string, start=0):
+
+    ptr = space(string, start)
+
+    if not string[ptr].isdigit():
+        return [ptr, None]
+
+    while string[ptr].isdigit():
+        ptr += 1 
+
+    value = int(string[start:ptr])
     b0 = (value >> 0 ) & 0xff
     b1 = (value >> 8 ) & 0xff 
     b2 = (value >> 16) & 0xff
     b3 = (value >> 24) & 0xff 
-
-    assm.code += [b0,b1,b2,b3]
-    assm.start = assm.pos
-    assm.ip += 4
-    return True
+    return [ptr, [b0, b1, b2, b3]]
 
 def comment(assm):
     white_space(assm)
@@ -265,6 +290,21 @@ def string_with_quotes(assm):
             assm.ip += 1
             return True
     return False
+
+def string(string, start=0):
+    ptr0 = space(string, start)
+
+    if string[ptr0] == '\"':
+        ptr0 += 1
+        ptr1 = ptr0
+        c = string[ptr1]
+        while (c.isalpha() or c.isdigit()):
+            ptr1 += 1
+            c = string[ptr1] 
+        if string[ptr1] == '\"':
+            ptr1 += 1
+            return [ptr1, string[ptr0: ptr1-1]]
+    return [start, None]
 
 def label_ref(assm):
     white_space(assm)
--- tests/test_stack_machine.py
+++ tests/test_stack_machine.py
@@ -7,6 +7,9 @@ def test_isub():
     assert assm.ip == 1 
     assert assm.code == [ISUB]
 
+def test_string():
+    assert string(""" "hello" """) == [8, 'hello']
+
 def test_comment():
     string = '; this is a comment\n'
     assm = Assembler('{0} next'.format(string))
@@ -21,12 +24,23 @@ def test_instruction_iadd():
     assert assm.ip == 1 
     assert assm.code == [IADD]
 
+def test_integer():
+    assert integer('235\n') == [3, [235, 0, 0, 0]]
+
 def test_instruction_ipush():
     assm = Assembler('ipush 56\n')
     instruction_ipush(assm)
     assert assm.pos == 8
     assert assm.ip == 5 
     assert assm.code == [IPUSH, 56, 0, 0, 0]
+
+def test_instruction_spush():
+    assm = Assembler("""spush "hello"\n""")
+    instruction_spush(assm)
+    assert assm.pos == 13
+    assert assm.ip == 2 
+    assert assm.code == [SPUSH, 0]
+    assert assm.constants == ['hello']
 
 def test_instruction_print():
     assm = Assembler('print\n')
