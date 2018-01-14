ae3a6b35bda0a4d6465bec9f0f34bd4127516e0c
jrreed83 <joeyrreed@gmail.com>
2018-01-13 21:09:31 -0800

    fixed tests

--- tests/test_stack_machine.py
+++ tests/test_stack_machine.py
@@ -1,55 +1,34 @@
 from assembler.parser import *
 
 def test_string():
-    s = stream(""" "hello" """)
-    s, token = quoted_string(s)
-    assert token == (Type.STRING, 'hello')
-    assert s['ptr'] ==  8 
+    n, token = quoted_string(""""hello"\n""")
+    assert (n, token) == (7, (Type.STRING, 'hello'))
 
 def test_comment():
-    s = stream('; this is a comment\n')
-    s, token = comment(s)
-    assert token == True
-    assert s['ptr'] ==  19
+    n, token = comment('; this is a comment\n')
+    assert (n, token) == (19, True)
 
 def test_label():
-    s = stream('test:\n')
-    s, token = label(s)    
-    assert token == (Type.LABEL, 'test')
-    assert s['ptr'] == 5
+    n, token = label('test:\n')    
+    assert (n, token) == (5, (Type.LABEL, 'test'))
 
 def test_iadd(): 
-    s = stream('iadd\n')
-    s, token = operation(s)    
-    assert token == Op.IADD
-    assert s['ptr'] == 4
+    n, token = operation('iadd\n')    
+    assert (n, token) == (4, Op.IADD)
 
 def test_integer():
-    s = stream('235\n')
-    s, token = integer(s)    
-    assert token == (Type.INTEGER, '235')
-    assert s['ptr'] == 3
+    n, token = integer('235\n')    
+    assert (n, token) == (3, (Type.INTEGER, '235'))
 
 def test_decimal():
-    s = stream('2.35\n')
-    s, token = decimal(s)    
-    assert token == (Type.DECIMAL, '2.35')    
-    assert s['ptr'] == 4
+    n, token = decimal('2.35\n')    
+    assert (n, token) == (4, (Type.DECIMAL, '2.35'))
 
 def test_ipush():
-    s = stream('ipush\n')
-    s, token = operation(s)    
-    assert token == Op.IPUSH
-    assert s['ptr'] == 5
+    n, token = operation('ipush\n')    
+    assert (n, token) == (5, Op.IPUSH)
 
 def test_print():
-    s = stream('print\n')
-    s, token = operation(s)    
-    assert token == Op.PRINT
-    assert s['ptr'] == 5
-
-def test_failed_op():
-    assert operation('foo\n', 0) == (0, None)
-
-def test_failed_statement():
-    assert statement('foo:\n     ', 0) == (0, None)    
+    n, token = operation('print\n')    
+    assert (n, token) == (5, Op.PRINT)
+  
